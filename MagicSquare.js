02222f9cfdec26c4e4ee2f107cfab02711d38668
YUGANDHAR2003 <85959898+YUGANDHAR2003@users.noreply.github.com>
2023-10-27 09:43:19 +0530

    MagicSquare.js

--- MagicSquare.js
+++ MagicSquare.js
@@ -3,15 +3,15 @@
  * @returns {number[][]} A 3x3 array representing the magic square.
  */
 function generateRandomMagicSquare() {
-    let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    let magicSquare;
+    let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    let magicSquare
 
     do {
-        numbers = shuffleArray(numbers); // Shuffle the numbers randomly.
-        magicSquare = createMagicSquare(numbers);
-    } while (!isValidMagicSquare(magicSquare));
+        numbers = shuffleArray(numbers) // Shuffle the numbers randomly.
+        magicSquare = createMagicSquare(numbers)
+    } while (!isValidMagicSquare(magicSquare))
 
-    return magicSquare;
+    return magicSquare
 }
 
 /**
@@ -21,10 +21,10 @@ function generateRandomMagicSquare() {
  */
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
+        let j = Math.floor(Math.random() * (i + 1))
+        ;[array[i], array[j]] = [array[j], array[i]]
     }
-    return array;
+    return array
 }
 
 /**
@@ -37,7 +37,7 @@ function createMagicSquare(numbers) {
         [numbers[0], numbers[1], numbers[2]],
         [numbers[3], numbers[4], numbers[5]],
         [numbers[6], numbers[7], numbers[8]]
-    ];
+    ]
 }
 
 /**
@@ -46,22 +46,22 @@ function createMagicSquare(numbers) {
  * @returns {boolean} True if it's a valid magic square, false otherwise.
  */
 function isValidMagicSquare(square) {
-    let targetSum = square[0].reduce((a, b) => a + b, 0);
+    let targetSum = square[0].reduce((a, b) => a + b, 0)
     for (let i = 0; i < 3; i++) {
         if (square[i].reduce((a, b) => a + b, 0) !== targetSum) {
-            return false;
+            return false
         }
         if (square[0][i] + square[1][i] + square[2][i] !== targetSum) {
-            return false;
+            return false
         }
     }
     if (square[0][0] + square[1][1] + square[2][2] !== targetSum) {
-        return false;
+        return false
     }
     if (square[0][2] + square[1][1] + square[2][0] !== targetSum) {
-        return false;
+        return false
     }
-    return true;
+    return true
 }
 
 /**
@@ -69,15 +69,15 @@ function isValidMagicSquare(square) {
  * @param {number[][]} grid - A 3x3 array representing the magic square.
  */
 function printTheGrid(grid) {
-    console.log("-------------");
+    console.log("-------------")
     for (let i = 0; i < grid.length; i++) {
-        let row = grid[i].map((num) => `| ${num} `).join('') + '|';
-        let separator = i === grid.length - 1 ? "-------------" : "----+---+---";
-        console.log(row);
-        console.log(separator);
+        let row = grid[i].map((num) => `| ${num} `).join('') + '|'
+        let separator = i === grid.length - 1 ? "-------------" : "----+---+---"
+        console.log(row)
+        console.log(separator)
     }
 }
 
 // Generate and print a random 3x3 magic square
-let magicSquare = generateRandomMagicSquare();
-printTheGrid(magicSquare);
+let magicSquare = generateRandomMagicSquare()
+printTheGrid(magicSquare)
